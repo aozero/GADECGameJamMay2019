4790c9cc9240117def9940c5e5652c0c0200de7d
Alexander Ozero <a.ozero@live.com>
2019-05-18 15:32:25 -0600

    Add boost depletion and replenishment
    Does not affect speed yet

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,22 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    private float waitTime = 2.0f;
-    private float timer = 0.0f;
-    
     public int maxHealth = 10;
     public Slider healthSlider;
+    
+    public int maxBoost = 200;
+    public Slider boostSlider;
 
     public float forwardSpeed;    // How fast the player can move forward
     public float turnSpeed;       // How fast the player can turn
     public BulletPlayer bulletPlayer;
 
     private int currentHealth;
+    private int currentBoost;
+    
+    private float waitTime = 2.0f;
+    private float timer = 0.0f;
+    
     private Rigidbody2D body;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
 
     public Vector2 Position
@@ -28,8 +33,9 @@ public class PlayerController : MonoBehaviour
 
     void Awake()
     {
-        // Set health to max
+        // Set health and boost to max
         currentHealth = maxHealth;
+        currentBoost = maxBoost;
 
         //Get and store a reference to the Rigidbody2D component so that we can access it.
         body = GetComponent<Rigidbody2D>();
@@ -45,6 +51,17 @@ public class PlayerController : MonoBehaviour
             Fire();
             timer = 0.0f;
         }
+        
+        if (Input.GetKey(KeyCode.LeftShift) && boostSlider.value > 0){
+            currentBoost -= 4;
+            boostSlider.value = currentBoost;
+        }
+        if (currentBoost < maxBoost && !Input.GetKey(KeyCode.LeftShift))
+        {
+            currentBoost += 1;
+            boostSlider.value = currentBoost;
+        }
+        
     }
 
     //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
