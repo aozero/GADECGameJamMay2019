a32c1f44c9055878eeb6cbdfebf727a9f0decd9c
IanWark <wark@ualberta.ca>
2019-05-18 18:29:31 -0600

    Add music playing on loop

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioClip homeMusic;
+    public AudioSource musicSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSource.loop = true;
+        musicSource.clip = homeMusic;
+        musicSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
